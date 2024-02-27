9e78a79899c10ac474c2acb3f0e97b5c4726e4f5
Diego Maranhão <diegomaranhao@outlook.com>
2024-02-26 21:56:01 -0400

    Update error Handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/snippet/view", snippetViewHandler)
-	mux.HandleFunc("/snippet/create", snippetCreateHandler)
+	mux.HandleFunc("/", app.homeHandler)
+	mux.HandleFunc("/snippet/view", app.snippetViewHandler)
+	mux.HandleFunc("/snippet/create", app.snippetCreateHandler)
 
-	log.Printf("Starting server on %s", *addr)
+	logger.Info("Starting server", "addr", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
 
+	logger.Error(err.Error())
+	os.Exit(1)
 }
